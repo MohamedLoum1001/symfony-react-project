2412c667af6708939b4cb9c8c950e1a855f4cd8f
MohamedLoum1001 <mloum137@gmail.com>
2026-08-20 13:00:10 +0200

    Fix: direct CORS handling at index.php entry

--- public/index.php
+++ public/index.php
@@ -1,9 +1,19 @@
 <?php
 
+// Gestion globale CORS et requêtes OPTIONS
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With, Accept");
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit(0);
+}
+
 use App\Kernel;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
-return static function (array $context) {
+return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
